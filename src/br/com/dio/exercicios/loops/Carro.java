67e0ad0a7270f044144a732cbf60b82f7c1a3a37
ZanonLuc <zanon626@gmail.com>
2022-02-14 17:55:58 -0300

    commit inicial

---
+++ src/br/com/dio/exercicios/loops/Carro.java
@@ -0,0 +1,8 @@
+package br.com.dio.exercicios.loops;
+
+public class Aula3 {
+    int modulo = 6;
+    int duracao = 10;
+    String didatica = "Muito boa";
+    
+}
---
+++ src/variaveis/Var.java
@@ -0,0 +1,2 @@
+package variaveis;public class Var {
+}
